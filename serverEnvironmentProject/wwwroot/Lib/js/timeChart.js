bd181e325d18b0e15153519520e2b03f943eca7f
Tobyjcode <tobias-jes@hotmail.com>
2024-02-14 10:25:04 +0100

    #Added frontend dashboard, Nice Google Charts and javascript data examples using google charts.

---
+++ serverEnvironmentProject/wwwroot/Lib/js/timeChart.js
@@ -0,0 +1,27 @@
+﻿//google.charts.load('current', {
+//    packages: ['corechart', 'line']
+//}).then(drawChart);
+
+//function drawChart() {
+//    var data = new google.visualization.DataTable();
+//    data.addColumn('number', 'Time');
+//    data.addColumn('number', 'Value');
+
+//    // Add your data here. For example:
+//    data.addRow([1, 100]);
+//    data.addRow([2, 120]);
+//    data.addRow([3, 140]);
+
+//    var options = {
+//        hAxis: {
+//            title: 'Time'
+//        },
+//        vAxis: {
+//            title: 'Value'
+//        },
+//        title: 'TimeChart'
+//    };
+
+//    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+//    chart.draw(data, options);
+//}
