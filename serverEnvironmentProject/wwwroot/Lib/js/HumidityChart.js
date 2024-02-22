0cc62b74d3f45a158f619c963e190577ea49f229
Tobyjcode <tobias-jes@hotmail.com>
2024-02-22 19:22:56 +0100

    # I have fixed the layout problems and Implemented responsive chart layout and sidebar navigation. Adjusted chart sizing to be dynamic based on container size.

--- serverEnvironmentProject/wwwroot/Lib/js/HumidityChart.js
+++ serverEnvironmentProject/wwwroot/Lib/js/HumidityChart.js
@@ -1,4 +1,3 @@
-// gaugeChart.js
 google.charts.load('current', { 'packages': ['gauge'] });
 
 function drawHumidityChart(label, value, elementId) {
@@ -8,20 +7,22 @@ function drawHumidityChart(label, value, elementId) {
             [label, value]
         ]);
 
+        var element = document.getElementById(elementId);
         var options = {
-            width: 200, height: 200,
-            redFrom: 200, redTo: 130,
+            width: element.offsetWidth,
+            height: element.offsetHeight,
+            redFrom: 90, redTo: 100,
             yellowFrom: 75, yellowTo: 90,
             minorTicks: 5
         };
 
-        var chart = new google.visualization.Gauge(document.getElementById(elementId));
+        var chart = new google.visualization.Gauge(element);
 
         chart.draw(data, options);
 
         setInterval(function () {
             data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
             chart.draw(data, options);
-        }, 13000);
+        }, 1000);
     });
 }
--- serverEnvironmentProject/wwwroot/Lib/js/TemperatureChart.js
+++ serverEnvironmentProject/wwwroot/Lib/js/TemperatureChart.js
@@ -1,4 +1,3 @@
-// TemperatureChart.js
 google.charts.load('current', { 'packages': ['gauge'] });
 
 function drawTemperatureChart(label, value, elementId) {
@@ -8,20 +7,22 @@ function drawTemperatureChart(label, value, elementId) {
             [label, value]
         ]);
 
+        var element = document.getElementById(elementId);
         var options = {
-            width: 900, height: 900,
+            width: element.offsetWidth,
+            height: element.offsetHeight,
             redFrom: 90, redTo: 100,
             yellowFrom: 75, yellowTo: 90,
             minorTicks: 5
         };
 
-        var chart = new google.visualization.Gauge(document.getElementById(elementId));
+        var chart = new google.visualization.Gauge(element);
 
         chart.draw(data, options);
 
         setInterval(function () {
             data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
             chart.draw(data, options);
-        }, 13000);
+        }, 1000);
     });
 }
